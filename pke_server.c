d324208875c8d03f6f1fc2df0f2cc341fa4624e7
pkmartus <pkmartus@gmail.com>
2025-10-13 12:40:21 -0400

    added imports and variable declarations to pke_server

--- pke_server.c
+++ pke_server.c
@@ -1,5 +1,30 @@
 #include "lodi_messages.h"
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-//process key registration
+void DieWithError(char *errorMessage);
 
-//procccss key requests
+int main(int argc, char * argv[])
+{
+    //local
+    int sock; //socket
+    struct sockaddr_in fromAddr;
+    unsigned short pkeServerPort;
+
+    //Lodi Client
+    struct sockaddr_in lodiClientAddr; //address
+    PClientToLodiServer clientRegisterKey; //buffer for register key message
+
+    //Lodi Server
+    struct sockaddr_in lodiServerAddr; //address
+    PClientToLodiServer serverRequestKey; //buffer for request PK message
+
+    
+    //process key registration
+
+    //procccss key requests
+}
