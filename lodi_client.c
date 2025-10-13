c92db0f13edecc11a8b868f8009b90e5abee364c
pkmartus <pkmartus@gmail.com>
2025-10-13 11:51:02 -0400

    added imports and variable declarations to lodi_client

--- lodi_client.c
+++ lodi_client.c
@@ -1,5 +1,38 @@
 #include "tfa_messages.h"
 #include "lodi_messages.h"
+#include <sys/socket.h> //needed to use socket(), connect(), sendto() and recvfrom()
+#include <arpa/inet.h> // sockaddr and inet_addr()
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h> //for close
+
+void DieWithError(char *errorMessage);
+
+int main(int argc, char *argv[]) //argc counts the arguments and argv contains them
+{
+    //initialize variables adapted from example code
+    int sock;
+    struct sockaddr_in pkeServAddr; //pke server address
+    unsigned short pkeServPort; //pke server port
+    char *pkeServIP; //the ip address of the server
+    PClientTOPKServer registerKey; //message to send to pke server
+    PKServerTOPClientOrLodiServer ackRegisterKey; //buffer for response from PKE server
+
+    struct sockaddr_in lodiServAddr; //lodi server address
+    unsigned short lodiServPort; //lodi server port
+    char *lodiServIP; //ip of lodi server
+    PClientToLodiServer login; //message to Lodi server
+    LodiServerToLodiClientAcks ackLogin; //buffer for response from lodi server
+
+    struct sockaddr_in fromAddr; // source address for responses
+    unsigned int fromSize; //the size of the address for recvfrom()
+    
+
+    
+
+
+
+
 
 // sumit users public key to PKE server
 
@@ -7,3 +40,4 @@
 
 //exit
 
+}
--- lodi_messages.h
+++ lodi_messages.h
@@ -4,23 +4,37 @@
 #define LODIMESSAGES_H
 
 /*sample code from handout*/
+
+//message from Lodi Client to Lodi Server
+typedef struct {
+    enum {login} messageType;
+    unsigned int userID; //user identifier
+    unsigned int recipientID; //message recipient identifier
+    unsigned long timestamp; //timestamp
+    unsigned long digitalSig; //encrypted timestamp
+} PClientToLodiServer;
+
+//message from Lodi server to lodi client
 typedef struct {
     enum {ackLogin} messageType;
     unsigned int userID;
 } LodiServerToLodiClientAcks;
 
+//message from PKE server to PKE client
 typedef struct {
     enum{ackRegisterKey, responsePublicKey} messageType;
     unsigned int userID; 
     unsigned int publicKey;
 } PKServerToLodiCLient;
 
+//Message from Lodi client to PKE server
 typedef struct {
     enum {registerKey, requestKey} messageType;
     unsigned int userID;
     unsigned int publicKey; //contains 0 if type is request_key
 } PClientTOPKServer;
 
+//Message from PKEserver to Lodi Client or LodiServer
 typedef struct {
     enum {ackRegistrerKey, responsePublicKey} messageType;
     unsigned int userID;
