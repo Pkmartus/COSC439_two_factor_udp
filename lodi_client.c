e5644dc63ede41b9355e46f4a5a9269402d49884
pkmartus <pkmartus@gmail.com>
2025-11-24 11:27:23 -0500

    added new scaffolding to client

--- lodi_client.c
+++ lodi_client.c
@@ -17,9 +17,10 @@ void DieWithError(char *errorMessage);
 int main(int argc, char *argv[]) // argc counts the arguments and argv contains them
 {
     // initialize variables adapted from example code
-    int lodiSock;                // socket (not sure if singular or one needed for each server used)
-    struct sockaddr_in fromAddr; // buffer for recieved messages
-    unsigned int fromSize;       // the size of the address for recvfrom() (may need to change this to be seperate for each server)
+    //UDP variables for auth
+    int udpSock;                // socket (not sure if singular or one needed for each server used)
+    struct sockaddr_in fromAddrUdp; // buffer for recieved messages
+    unsigned int fromSizeUdp;       // the size of the address for recvfrom() (may need to change this to be seperate for each server)
     unsigned int userID;
 
     // PKE variables
@@ -31,13 +32,16 @@ int main(int argc, char *argv[]) // argc counts the arguments and argv contains 
     unsigned int ackRegSize;               // size of the ackRegisterKey message
 
     //todo change to work with tcp
+    int tcpSock;
+
     // Lodi Server variables
     struct sockaddr_in lodiServAddr;            // lodi server address
     unsigned short lodiServPort;                // lodi server port
     char *lodiServIP;                           // ip of lodi server
     PClientToLodiServer loginMessage;           // message to Lodi server
-    LodiServerMessage ackLoginMessage; // buffer for response from lodi server
+    LodiServerMessage ackBuffer[sizeof(LodiServerMessage)]; // buffer for response from lodi server
     unsigned int ackLoginSize;                  // length of the acknowlegment message
+    int bytesRcvd, totalBytesRcvd;              //bytes recieved in a single recv() and total bytes recieved
 
     // get user ID
     printf("[Lodi_Client] Please enter user ID: \n");
@@ -51,7 +55,7 @@ int main(int argc, char *argv[]) // argc counts the arguments and argv contains 
     printf("[Lodi_Client] computed public key: %d\n", publicKey);
 
     // Create a datagram/UDP socket
-    if ((lodiSock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+    if ((udpSock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
         DieWithError("[Lodi_Client] socket() failed");
 
     //set ip and port for pke server
@@ -71,23 +75,23 @@ int main(int argc, char *argv[]) // argc counts the arguments and argv contains 
     pkeServAddr.sin_port = htons(pkeServPort);
 
     // send primary key to pke server
-    if (sendto(lodiSock, (void *)&registerKeyMessage, sizeof(registerKeyMessage), 0,
+    if (sendto(udpSock, (void *)&registerKeyMessage, sizeof(registerKeyMessage), 0,
                (struct sockaddr *)&pkeServAddr, sizeof(pkeServAddr)) != sizeof(registerKeyMessage))
         DieWithError("[Lodi_Client] sendto() sent a different number of bytes than expected");
     printf("[Lodi_Client] Register -> public key: %u with PKE server\n", publicKey);
 
 
     // recieve acknowlegement from pke server
-    fromSize = sizeof(fromAddr);
+    fromSizeUdp = sizeof(fromAddrUdp);
     ackRegSize = sizeof(ackRegisterKeyMessage);
     memset(&ackRegisterKeyMessage, 0, ackRegSize); // zero out structure
 
-    if ((ackRegSize = recvfrom(lodiSock, (void *)&ackRegisterKeyMessage, ackRegSize, 0,
-                               (struct sockaddr *)&fromAddr, &fromSize)) < 0)
+    if ((ackRegSize = recvfrom(udpSock, (void *)&ackRegisterKeyMessage, ackRegSize, 0,
+                               (struct sockaddr *)&fromAddrUdp, &fromSizeUdp)) < 0)
         DieWithError("[Lodi_Client] Login Acknowlegement from the server failed.");
 
     // check that response came from correct server
-    if (pkeServAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr)
+    if (pkeServAddr.sin_addr.s_addr != fromAddrUdp.sin_addr.s_addr)
         DieWithError("[Lodi_Client] Packet from unknown source");
     printf("[Lodi_Client] Response <- PKE Server Public Key: %u\n", ackRegisterKeyMessage.publicKey);
 
@@ -103,15 +107,15 @@ int main(int argc, char *argv[]) // argc counts the arguments and argv contains 
     // set server port
     lodiServPort = LODI_DEFAULT_PORT;
 
-    // set message varriables
-    memset(&loginMessage, 0, sizeof(loginMessage));
-    loginMessage.messageType = login;
-    loginMessage.userID = userID;
-    loginMessage.recipientID = 0;
-    unsigned long currentTime = reduceInput((long)time(NULL));
-    loginMessage.timestamp = currentTime;
-    unsigned long digitalSig = rsaEncrypt(currentTime, privateKey); 
-    loginMessage.digitalSig = digitalSig;
+    // // set message varriables
+    // memset(&loginMessage, 0, sizeof(loginMessage));
+    // loginMessage.messageType = login;
+    // loginMessage.userID = userID;
+    // loginMessage.recipientID = 0;
+    // unsigned long currentTime = reduceInput((long)time(NULL));
+    // loginMessage.timestamp = currentTime;
+    // unsigned long digitalSig = rsaEncrypt(currentTime, privateKey); 
+    // loginMessage.digitalSig = digitalSig;
 
     // set lodi server address structure
     memset(&lodiServAddr, 0, sizeof(lodiServAddr));       // zero out structure
@@ -119,45 +123,69 @@ int main(int argc, char *argv[]) // argc counts the arguments and argv contains 
     lodiServAddr.sin_addr.s_addr = inet_addr(lodiServIP); // lodi server ip
     lodiServAddr.sin_port = htons(lodiServPort);          // lodi server port
 
-    // send login request to Lodi Server
-    if (sendto(lodiSock, (void *)&loginMessage, sizeof(loginMessage), 0, (struct sockaddr *)&lodiServAddr, sizeof(lodiServAddr)) != sizeof(loginMessage))
-        DieWithError("[Lodi_Client] sendto() sent a different number of bytes than expected");
-    printf("[Lodi_Client] Request -> Login User: %u, Current time: %lu, Digital Signature: %lu to lodi server\n", userID, currentTime, digitalSig);
+    // // send login request to Lodi Server
+    // if (sendto(udpSock, (void *)&loginMessage, sizeof(loginMessage), 0, (struct sockaddr *)&lodiServAddr, sizeof(lodiServAddr)) != sizeof(loginMessage))
+    //     DieWithError("[Lodi_Client] sendto() sent a different number of bytes than expected");
+    // printf("[Lodi_Client] Request -> Login User: %u, Current time: %lu, Digital Signature: %lu to lodi server\n", userID, currentTime, digitalSig);
 
-    // Recieve the response
-    fromSize = sizeof(fromAddr);
-    ackLoginSize = sizeof(ackLoginMessage);
-    memset(&ackLoginMessage, 0, ackLoginSize); // zero out structure
+    // // Recieve the response
+    // fromSizeUdp = sizeof(fromAddrUdp);
+    // ackLoginSize = sizeof(ackBuffer);
+    // memset(&ackBuffer, 0, ackLoginSize); // zero out structure
 
-    if ((ackLoginSize = recvfrom(lodiSock, (void *)&ackLoginMessage, ackLoginSize, 0,
-                                 (struct sockaddr *)&fromAddr, &fromSize)) < 0)
-        DieWithError("[Lodi_Client] Login Acknowlegement from the server failed.");
+    // if ((ackLoginSize = recvfrom(udpSock, (void *)&ackBuffer, ackLoginSize, 0,
+    //                              (struct sockaddr *)&fromAddrUdp, &fromSizeUdp)) < 0)
+    //     DieWithError("[Lodi_Client] Login Acknowlegement from the server failed.");
 
-    // check that response came from correct server
-    if (lodiServAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr)
-        DieWithError("[Lodi_Client] Packet from unknown source");
+    // // check that response came from correct server
+    // if (lodiServAddr.sin_addr.s_addr != fromAddrUdp.sin_addr.s_addr)
+    //     DieWithError("[Lodi_Client] Packet from unknown source");
 
-    printf("[Lodi_Client] Response <- from Lodi server, Login Successful \n");
+    // printf("[Lodi_Client] Response <- from Lodi server, Login Successful \n");
 
     //todo expand menu to include menu options.
-    int quit = 1;
-    while(quit) {
-        //TODO login
+    int option = 9; //default to 9 because it's not an option on the list
+    while(option) {
+        printf("[Lodi Client] select an option:\n");
+        printf("1. Login\n");
+        printf("2. Make Post\n");
+        printf("3. Request Feed\n");
+        printf("4. Follow an Idol\n");
+        printf("5. Unfollow an Idol\n");
+        printf("6. Logout\n");
+        printf("0. Quit\n");
 
-        //TODO post
+        scanf("%d", &option);
 
-        //TODO request feed of messages from followed idols
-
-        //TODO follow an idol
-
-        //TODO unfollow an idol
-
-        //TODO logout
-        printf("[Lodi Client] press 0 to exit\n");
-        scanf("%d", &quit);
+        switch(option) {
+            case 1:
+                //TODO login
+                break;
+            case 2:
+                //TODO post
+                break;
+            case 3:
+                //TODO request feed of messages from followed idols
+                break;
+            case 4:
+                //TODO follow an idol
+                break;
+            case 5:
+                //TODO unfollow an idol
+                break;
+            case 6:
+                //TODO logout
+                break;
+            case 0:
+                //TODO Quit
+                //should logout as well
+                break;
+            default:
+                printf("[Lodi Client] invalid output try again");
+        }
     }
 
     // exit
-    close(lodiSock);
+    close(udpSock);
     exit(0);
 }
--- lodi_messages.h
+++ lodi_messages.h
@@ -20,11 +20,10 @@ typedef struct {
 
 //message from Lodi server to lodi client
 typedef struct {
-    enum {ackLogin, ackPost, ackFeed, ackFollow, ackUnfollow, ackLogout } messageType;
+    enum {ackLogin, ackPost, ackFeed, ackFollow, ackUnfollow, ackLogout, feedMessage} messageType;
     unsigned int userID; /* unique client identifier */
     char message[100]; /* posted text message */
-    unsigned int next; //my addition for determining how many messages in feed contains the number of messages after the current one, contains 0 if last message, contains 0 if any other type than ackFeed
-    //may need to break out into it's own message type if determining size in advance becomes an issue
+    unsigned int next; //my addition, used in ackFeed to send the number of messages coming in the feed. send type feed message after that
 } LodiServerMessage; /* an unsigned int is 32 bits = 4 bytes */
 
 
